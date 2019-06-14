f212df8f326ab69b9eda14dc9dc053f1f41b2ccd
GenaBitu <developer@dedic.eu>
2019-06-14 12:07:41 +0200

    Removed webpack public path completely

--- assets/dev/webpack.config.js
+++ assets/dev/webpack.config.js
@@ -41,8 +41,7 @@ module.exports = {
     },
     output: {
         filename: '[name].js',
-        path: path.resolve(__dirname + './../', 'js'),
-        publicPath: path.resolve(__dirname + './../'),
+        path: path.resolve(__dirname + './../', 'js')
     },
     devtool: 'source-map',
     watchOptions: {
